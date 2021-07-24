155fc64142d964e48ee20e51d29c07c8ebe6f6f2
yungshun317 <seraph317@gmail.com>
2021-07-24 19:33:15 +0800

    [FEAT] Create an about section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 
-import Section from "./Section";
-
 export default function App() {
     return (
         <>
-        <div className="bg-light">
+            <div className="bg-light">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-sm-12 bg-light">
+                            <div>
+                                <h2 style={{
+                                    display: "block",
+                                    marginBlockStart: "16px",
+                                    marginBlockEnd: "16px"
+                                }}>Lorem Ipsum</h2>
+                                <p style={{
+                                    display: "block",
+                                    marginBlockStart: "16px",
+                                    marginBlockEnd: "16px"
+                                }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus quis massa vel tempus. Nunc laoreet dolor sit amet massa pulvinar, ac tempor nibh pretium. Quisque interdum dolor eu varius varius.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6 col-sm-12 bg-light">
-                        <div>
+                    <div className="col-md-6 col-sm-12">
+                        <div className="bg-danger">
                             <h2 style={{
                                 display: "block",
                                 marginBlockStart: "16px",
@@ -19,14 +36,18 @@ export default function App() {
                                 display: "block",
                                 marginBlockStart: "16px",
                                 marginBlockEnd: "16px"
-                            }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus quis massa vel tempus. Nunc laoreet dolor sit amet massa pulvinar, ac tempor nibh pretium. Quisque interdum dolor eu varius varius.</p>
+                            }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus quis massa vel tempus. Nunc
+                                laoreet dolor sit amet massa pulvinar, ac tempor nibh pretium. Quisque interdum dolor eu
+                                varius varius.</p>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                        <div className="bg-success">
+                            <img src="https://picsum.photos/200/300"/>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <Section>
-        </Section>
         </>
     );
 }
