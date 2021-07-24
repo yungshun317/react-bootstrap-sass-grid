62c4967ffc81fc7f9736410f140fc9612e04123f
yungshun317 <seraph317@gmail.com>
2021-07-24 19:35:53 +0800

    [FEAT] Create three equal columns

--- src/App.js
+++ src/App.js
@@ -48,6 +48,49 @@ export default function App() {
                     </div>
                 </div>
             </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 col-sm-12">
+                        <div className="bg-danger">
+                            <img src="https://picsum.photos/200/300" style="width: 100%"/>
+                            <div style="overflow: auto; margin-left: 40px; margin-right: 40px">
+                                <h2 style="display: block; margin-block-start: 16px; margin-block-end: 16px">Lorem
+                                    Ipsum</h2>
+                                <p style="display: block; margin-block-start: 16px; margin-block-end: 16px">Lorem ipsum
+                                    dolor sit amet, consectetur adipiscing elit. Aliquam finibus quis massa vel tempus.
+                                    Nunc laoreet dolor sit amet massa pulvinar, ac tempor nibh pretium. Quisque interdum
+                                    dolor eu varius varius.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <div className="bg-success">
+                            <img src="https://picsum.photos/200/300" style="width: 100%"/>
+                            <div style="overflow: auto; margin-left: 40px; margin-right: 40px">
+                                <h2 style="display: block; margin-block-start: 16px; margin-block-end: 16px">Lorem
+                                    Ipsum</h2>
+                                <p style="display: block; margin-block-start: 16px; margin-block-end: 16px">Lorem ipsum
+                                    dolor sit amet, consectetur adipiscing elit. Aliquam finibus quis massa vel tempus.
+                                    Nunc laoreet dolor sit amet massa pulvinar, ac tempor nibh pretium. Quisque interdum
+                                    dolor eu varius varius.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <div className="bg-primary">
+                            <img src="https://picsum.photos/200/300" style="width: 100%"/>
+                            <div style="overflow: auto; margin-left: 40px; margin-right: 40px">
+                                <h2 style="display: block; margin-block-start: 16px; margin-block-end: 16px">Lorem
+                                    Ipsum</h2>
+                                <p style="display: block; margin-block-start: 16px; margin-block-end: 16px">Lorem ipsum
+                                    dolor sit amet, consectetur adipiscing elit. Aliquam finibus quis massa vel tempus.
+                                    Nunc laoreet dolor sit amet massa pulvinar, ac tempor nibh pretium. Quisque interdum
+                                    dolor eu varius varius.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
